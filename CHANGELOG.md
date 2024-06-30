d1ab53f376e58e4516bef17a58ff77f95696edcb
ChewyDev <dev@poodoge.net>
2024-06-30 17:10:45 -0400

    fix: viem version overrides for correct native currency on Web3Modal

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## 0.1.1 (2024-30-06)
+
+### fix: includes fix for native currency name for Dogechain
+
+- Bumped dependencies to latest versions
+- Added pnpm overrides for wagmi and viem to force Web3Modal to use the latest versions which fixes Dogechain chain description correcting native currency name and symbol to WDOGE
+
+
+## 0.1.0
+
+### Initial Release
+
+- Initial release of Doggo Dapp Starter Kit
--- constants/tokens/index.ts
+++ constants/tokens/index.ts
@@ -3,10 +3,10 @@ export const defaultTokens = {
             address: '0x2761723006d3Eb0d90B19B75654DbE543dcd974f',
             image: 'https://raw.githubusercontent.com/ChewySwap/chewy-tokenlist/main/tokens/chewy.png' //optional
         },
-        2000: {
+ /*   2000: {
             address: '0x2BE0096B24343549E34224aa9aa297E99961023D',
             image: 'https://raw.githubusercontent.com/PooDoge/dogeshrek-tokenlist/main/tokens/dogeShrek.png' //optional
-        }
+        } */
 }
 
 export default defaultTokens;
